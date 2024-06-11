97bb460f8c5b6d6d04202e2916fe84a439a4e226
Daniel Antonio Tala de Dompierre de Chaufepie <daniel.talaman@gmail.com>
2024-06-11 18:48:09 +0000

    feat: Refactor code to optimize museum selection for daily visits

--- app.py
+++ app.py
@@ -66,14 +66,43 @@ for day, museums in clusters.items():
 
 # reduce the number of museums to visit per day based on the visit_time (an int that 1 is an hour), the maximum hours per day is 8\
 
-for day, museums in clusters.items():
-    total_hours = 0
+# for day, museums in clusters.items():
+#     total_hours = 0
+#     for museum in museums:
+#         total_hours += museum['visit_time']
+#     while total_hours > 4:
+#         max_museum = max(museums, key=lambda x: x['visit_time'])
+#         museums.remove(max_museum)
+#         total_hours -= max_museum['visit_time']
+
+from math import radians, cos, sin, asin, sqrt
+
+def calculate_distance(museum1, museum2):
+    # Convert latitude and longitude from degrees to radians
+    lat1, lon1 = map(radians, [museum1['latitude'], museum1['longitude']])
+    lat2, lon2 = map(radians, [museum2['latitude'], museum2['longitude']])
+
+    # Haversine formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * asin(sqrt(a))
+    r = 6371  # Radius of earth in kilometers
+    return c * r
+
+def get_closest_museums(museums, max_distance, max_time):
+    closest_museums = []
+    total_time = 0
     for museum in museums:
-        total_hours += museum['visit_time']
-    while total_hours > 4:
-        max_museum = max(museums, key=lambda x: x['visit_time'])
-        museums.remove(max_museum)
-        total_hours -= max_museum['visit_time']
+        if all(calculate_distance(museum, other) <= max_distance for other in closest_museums):
+            if total_time + museum['visit_time'] <= max_time:
+                closest_museums.append(museum)
+                total_time += museum['visit_time']
+    return closest_museums
+
+# Reduce the number of museums to visit per day based on distance and visit time
+for day, museums in clusters.items():
+    clusters[day] = get_closest_museums(museums, max_distance=10, max_time=5)  # Replace 10 and 8 with your desired maximum distance and time
 
 for day, museums in clusters.items():
     print(f"Day recalculated {day + 1}:")
@@ -110,3 +139,9 @@ for day, museums in clusters.items():
     route = get_route(museums)
     for step in route[0]['legs'][0]['steps']:
         print(step['html_instructions'])
+
+
+# TODO: Add the code to display the route on a map
+# TODO: fix more than 2 museums per day
+# TODO: priority
+
